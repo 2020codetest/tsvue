00087f05772b9575f8d4d66c47eaf8ac62d359e4
David Zeng <davzen@microsoft.com>
2020-10-07 22:32:23 +0800

    add diff algorithm

--- src/Component.ts
+++ src/Component.ts
@@ -8,6 +8,7 @@ export interface Component{
     render(): RenderComponent;
     flush(parent: Node): Node;
     unmount(): void;
+    update(newComp: RenderComponent): void;
 }
 
 export type RenderComponent = Component | null;
@@ -42,6 +43,10 @@ export class TextWrapper implements Component{
         return this.node
     }
 
+    update(newComp: RenderComponent){
+        (this.node as Text).textContent = (newComp as TextWrapper).content
+    }
+
     unmount(): void{
     }
 }
@@ -51,6 +56,7 @@ export class ElementWrapper implements Component{
     type: string;
     props: any;
     node: Node;
+    parent: Node;
     listeners: {[index: string]: any};
     constructor(tag: string, props: any, children: Component[]){
         this.children = children;
@@ -59,8 +65,7 @@ export class ElementWrapper implements Component{
         this.listeners = {}
     }
 
-
-    unmount(): void{
+    releaseEvents(){
         for (let key in this.listeners){
             this.node.removeEventListener(key, this.listeners[key])
         }
@@ -68,11 +73,73 @@ export class ElementWrapper implements Component{
         this.listeners = {}
     }
 
+    updateProps(){
+        if (this.props){
+            for (let prop in this.props){
+                let value = this.props[prop]
+                if (prop === "className"){
+                    (this.node as HTMLElement).setAttribute("class", value)
+                }
+                else if (prop.match(/^on([\s\S]+)/)){
+                    let event = prop.substr(2).toLowerCase()
+                    this.listeners[event] = value
+                    this.node.addEventListener(event, this.listeners[event])
+                    continue;
+                }
+                else{
+                    (this.node as HTMLElement).setAttribute(prop, value)
+                }
+            }
+        }
+    }
+
+    unmount(): void{
+        this.releaseEvents()
+    }
+
     render(): RenderComponent{
         return this;
     }
 
+    update(newComp: Component): void{
+        if (newComp.type !== this.type){
+            // different type, just flush the whole tree
+            this.parent.removeChild(this.node)
+            this.node = newComp.flush(this.parent)
+        }
+        else{
+            this.releaseEvents()
+            this.updateProps()
+            // check children here
+            let minLength = Math.min(this.children.length, newComp.children.length)
+            for (let inx = 0; inx < minLength; ++inx){
+                // try to update same node
+                this.children[inx].update(newComp.children[inx].render())
+            }
+
+            // if there are more children in original comp, just remove them
+            if (this.children.length > minLength){
+                for (let inx = minLength; inx < this.children.length; ++inx){
+                    this.node.removeChild(this.children[inx].node)
+                }
+
+                this.children.splice(minLength, this.children.length - minLength)
+            }
+            // if there are more children in new comp, just flush them
+            else if (newComp.children.length > minLength){
+                for (let inx = minLength; inx < newComp.children.length; ++inx){
+                    newComp.children[inx].flush(this.node)
+                    this.children.push(newComp.children[inx])
+                }
+            }
+        }
+
+        this.type = newComp.type
+        this.props = newComp.props
+    }
+
     flush(parent: Node): Node{
+        this.parent = parent;
         let node = document.createElement(this.type)
         if (this.node){
             this.unmount()
@@ -83,24 +150,7 @@ export class ElementWrapper implements Component{
         }
 
         this.node = node
-        if (this.props){
-            for (let prop in this.props){
-                let value = this.props[prop]
-                if (prop === "className"){
-                    node.setAttribute("class", value)
-                }
-                else if (prop.match(/^on([\s\S]+)/)){
-                    let event = prop.substr(2).toLowerCase()
-                    this.listeners[event] = value
-                    node.addEventListener(event, this.listeners[event])
-                    continue;
-                }
-                else{
-                    node.setAttribute(prop, value)
-                }
-            }
-        }
-
+        this.updateProps()
         for (let child of this.children){
             child.flush(node)
         }
@@ -119,6 +169,7 @@ export class Vue<Data = {}, Props = {}> implements Component, INotifier{
     promiseIssued: boolean;
     watched: boolean;
     renderedComp?: Component;
+    cachedComp?: Component;
     constructor(props: Props) {
         this.children = [];
         this.type = "#custom";
@@ -156,7 +207,15 @@ export class Vue<Data = {}, Props = {}> implements Component, INotifier{
     flush(parent: Node): Node{
         this.unmount()
         this.parent = parent;
-        this.renderedComp = this.render()
+        // maybe it could be fetched from cache
+        if (this.cachedComp){
+            this.renderedComp = this.cachedComp
+            this.cachedComp = null
+        }
+        else{
+            this.renderedComp = this.render()
+        }
+
         let node = this.renderedComp.flush(parent)
         if (this.node){
             this.parent.replaceChild(node, this.node)
@@ -173,15 +232,17 @@ export class Vue<Data = {}, Props = {}> implements Component, INotifier{
         return this.node
     }
 
-    update(): void {
-        this.flush(this.parent)
+    update(newComp: Component): void {
+        this.renderedComp.update(newComp)
     }
 
     notify() {
         if (!this.promiseIssued){
             this.promiseIssued = true
             Promise.resolve().then(() => {
-                this.update()
+                this.cachedComp = this.render()
+                this.update(this.cachedComp)
+                this.cachedComp = null
                 this.promiseIssued = false
             })
         }
